fc1b1d080dc4eb8d91c59b2facd10a5373a4a971
hjdtl <873804682@qq.com>
2018-07-17 11:05:39 +0800

    test

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import App from '@/App'
 
-describe('HelloWorld.vue', () => {
+describe('App.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(App)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
+    expect(vm.$el.querySelector('.log').textContent)
+      .toEqual('</HJDTL>')
   })
 })
